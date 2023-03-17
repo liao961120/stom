2074b3a03ef5aadd818b9ae4c3f130dfb38f3709
Yongfu Liao <liao961120@gmail.com>
2023-03-17 17:04:03 +0800

    truncated normal distribution

--- irt/utils.R
+++ irt/utils.R
@@ -26,6 +26,18 @@ simple_hist = function( x, ylab="Count", lwd=5, col=2, ... ) {
 }
 
 
+# Truncated normal
+rtnorm = function(n, m=0, s=1, lower=0, upper=Inf) {
+  n_sampled = 0
+  sampled = c()
+  while (n_sampled < n) {
+    x = rnorm( n, m, s )
+    sampled = c( sampled, x[x > lower & x < upper] )
+    n_sampled = n_sampled + length(sampled)
+  }
+  sampled[seq_along(n)]
+}
+
 
 
 #' Posterior sample summaries
