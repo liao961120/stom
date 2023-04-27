fde627641e663ed8c0e1e3361bcf0c2b295d2ea0
Yongfu Liao <liao961120@gmail.com>
2023-04-27 11:58:53 +0800

    new model test

--- inst/wineNetwork/wine_first_level.R
+++ inst/wineNetwork/wine_first_level.R
@@ -21,7 +21,7 @@ sim_dat = function( Int=matrix(rep(0,4), nrow=2, byrow=T), diff_by_Ow=T ) {
   #   If Judge/Wine have same origin, an addition latent score of 1.3 is added.
   # Int = matrix(c(0, 0,
   #                1.3, 0 ), nrow=2, byrow=T )
-  
+
   d = expand.grid( Jid=1:Nj, Wid=1:Nw, KEEP.OUT.ATTRS=F )
   d$J = J[d$Jid]
   d$W = W[d$Wid]
@@ -29,7 +29,7 @@ sim_dat = function( Int=matrix(rep(0,4), nrow=2, byrow=T), diff_by_Ow=T ) {
   d$Ow = Ow[d$Wid]
   d$L = sapply( 1:nrow(d), function(i) d$J[i] + d$W[i] + Int[d$Oj[i], d$Ow[i]] )
   d$R = rbern( logistic(d$L) )
-  
+
   dat = list(
     N = Nj*Nw,
     Nw = Nw,
@@ -51,21 +51,21 @@ sim_dat = function( Int=matrix(rep(0,4), nrow=2, byrow=T), diff_by_Ow=T ) {
 
 
 ################################################
-# Model 1                     
-# 
+# Model 1
+#
 # [DGM]
 #   - [x] interaction
 #   - [0] Wine quality different by origin (Ow)
 # [Model]
 #   mu = W + J
-#   - [x] interaction 
+#   - [x] interaction
 #   - [x] Wine quality different by origin (Ow)
 # [Expect]
 #   W/J should be correctly recovered
 # [Results]
 #   As expected
 ################################################
-m = cmdstan_model("wine2_normal_flat.stan")
+m = cmdstan_model("wine2_normal_first_level.stan")
 (Int = matrix( c( 0, 0,
                   0, 0), nrow=2, byrow=T ) )
 dat = sim_dat(Int, diff_by_Ow = T)
@@ -79,21 +79,21 @@ plot( dat$J.ori, s1[grepl("J", s1$variable), "mean"] ); abline(0, 1)
 
 
 ################################################
-# Model 2                    
-# 
+# Model 2
+#
 # [DGM]
 #   - [o] interaction
 #   - [x] Wine quality different by origin (Ow)
 # [Model]
 #   mu = W + J + Int
-#   - [o] interaction 
+#   - [o] interaction
 #   - [x] Wine quality different by origin (Ow)
 # [Expect]
 #   W/J/Int should be correctly recovered
 # [Results]
 #   As expected
 ################################################
-m = cmdstan_model("wine2_normal_flat2.stan")
+m = cmdstan_model("wine2_normal_first_level2.stan")
 (Int = matrix( c( 1.5, 0,
                   1.5, 0), nrow=2, byrow=T ) )
 dat = sim_dat(Int, diff_by_Ow = F)
@@ -106,21 +106,21 @@ plot( Int, s[grepl("Int", s$variable), "mean"] ); abline(0, 1)
 
 
 ################################################
-# Model 3                    
-# 
+# Model 3
+#
 # [DGM]
 #   - [o] interaction
 #   - [o] Wine quality different by origin (Ow)
 # [Model]
 #   mu = W + J + Int
-#   - [o] interaction 
+#   - [o] interaction
 #   - [x] Wine quality different by origin (Ow)
 # [Expect]
 #   W/J/Int should be correctly recovered
 # [Results]
 #   Failed to recover W/J/Int
 ###############################################
-m = cmdstan_model("wine2_normal_flat2.stan")
+m = cmdstan_model("wine2_normal_first_level2.stan")
 (Int = matrix( c( 1.5, 0,
                   0,   0), nrow=2, byrow=T ) )
 dat = sim_dat(Int, diff_by_Ow = T)
@@ -137,21 +137,21 @@ plot( Int, s[grepl("Int", s$variable), "mean"] ); abline(0, 1)
 
 
 ################################################
-# Model 4                  
-# 
+# Model 4
+#
 # [DGM]
 #   - [o] interaction
 #   - [o] Wine quality different by origin (Ow)
 # [Model]
 #   mu = W + J + Ow + Int
-#   - [o] interaction 
+#   - [o] interaction
 #   - [o] Wine quality different by origin (Ow)
 # [Expect]
 #   (W+Ow)/Ow/J/Int should be correctly recovered
 # [Results]
 #   As expected
 #################################################
-m = cmdstan_model("wine2_normal_flat4.stan")
+m = cmdstan_model("wine2_normal_first_level4.stan")
 (Int = matrix( c( 1.5, 0,
                   0,   0), nrow=2, byrow=T ) )
 dat = sim_dat(Int, diff_by_Ow = T)
@@ -159,6 +159,34 @@ f4 = m$sample(data=dat, seed=123, chains=4, parallel_chains=4, refresh=500)
 s = f4$summary(variables=c("Wr", "J", "Int", "a"), "mean", "sd", "quantile2", "rhat", "ess_bulk")
 ( s = as.data.frame(s) )
 plot( dat$W.ori, s[grepl("Wr", s$variable), "mean"] ); abline(0, 1)
+plot( dat$J.ori, s[grepl("J", s$variable), "mean"] ); abline(0, 1)
+plot( Int, s[grepl("Int", s$variable), "mean"] ); abline(0, 1)
+
+
+################################################
+# Model 5
+#
+# [DGM]
+#   - [o] interaction
+#   - [x] Wine quality different by origin (Ow)
+# [Model]
+#   mu = W + J + Ow + Int
+#   - [o] interaction
+#   - [o] Wine quality different by origin (Ow)
+# [Expect]
+#   W/J/Ow(=0)/Int should be correctly recovered
+# [Results]
+#   As expected
+#################################################
+m = cmdstan_model("wine2_normal_first_level4.stan")
+(Int = matrix( c( 1.5, 0,
+                  0,   0), nrow=2, byrow=T ) )
+dat = sim_dat(Int, diff_by_Ow = F)
+f5 = m$sample(data=dat, seed=123, chains=4, parallel_chains=4, refresh=500)
+s = f5$summary(variables=c("Wr", "W", "J", "Int", "a"), "mean", "sd", "quantile2", "rhat", "ess_bulk")
+( s = as.data.frame(s) )
+plot( dat$W.ori, s[grepl("Wr", s$variable), "mean"] ); abline(0, 1)
+plot( dat$W.ori, s[grepl("W[^r]", s$variable), "mean"] ); abline(0, 1)
 plot( dat$J.ori, s[grepl("J", s$variable), "mean"] ); abline(0, 1)
 plot( Int, s[grepl("Int", s$variable), "mean"] ); abline(0, 1)
 
