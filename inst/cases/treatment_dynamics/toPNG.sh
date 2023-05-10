0e9b3a563288165f110b56cd62625264222cd971
Yongfu Liao <liao961120@gmail.com>
2023-05-10 17:19:02 +0800

    update

---
+++ inst/cases/treatment_dynamics/toPNG.sh
@@ -0,0 +1,2 @@
+inkscape --export-background-opacity=0 --export-dpi=1200 --export-type=png --pdf-poppler dag.pdf
+# https://superuser.com/questions/1686847/converting-pdf-to-image-via-inkscape-breaks-fonts
