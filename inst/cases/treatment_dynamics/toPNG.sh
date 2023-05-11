ab5fb60ce73d4e6760905dab89ad65117c1ce856
Yongfu Liao <liao961120@gmail.com>
2023-05-11 08:14:53 +0800

    inkscape code to convert from PDF to various formats

--- inst/cases/treatment_dynamics/toPNG.sh
+++
@@ -1,2 +0,0 @@
-inkscape --export-background-opacity=0 --export-dpi=1200 --export-type=png --pdf-poppler dag.pdf
-# https://superuser.com/questions/1686847/converting-pdf-to-image-via-inkscape-breaks-fonts
