5ff3a4a4747cf504eea1e9a084fb3a8ba223641e
Yongfu Liao <liao961120@gmail.com>
2023-07-24 11:57:26 +0800

    tweak doc

--- inst/cases/wine_network/wine.R
+++ inst/cases/wine_network/wine.R
@@ -4,6 +4,7 @@
 #' ---
 #' title: Joint Estimation of Interaction and Piped Effects in an Item Response Model
 #' author: "Yongfu Liao"
+#' date: "April 27, 2023"
 #' ---
 #'
 #' Description
@@ -19,7 +20,7 @@
 #' 3. $O_w$: Wine origin
 #'
 #' Furthermore, it is assumed that $O_w$ differentially influences $R$
-#' based on the levels of another variable $O_j$, the origin of the judge
+#' based on the levels of another variable $O_j$, the origin of the judge.
 #' In stats jargon, there's an interaction between $O_w$ and $O_j$. In simpler
 #' conceptual terms, consider the scenario that, for instance, French wines are
 #' rated higher by French judges, in addition to the scores they should have
