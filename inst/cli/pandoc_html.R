5d6fa111ea5209cd45bb673931c01d95dd1d2975
Yongfu Liao <liao961120@gmail.com>
2023-12-19 11:39:33 +0800

    Enhance command line interface

--- inst/cli/pandoc_html.R
+++ inst/cli/pandoc_html.R
@@ -1,13 +1,43 @@
-# Call as
-#   Rscript  this.R  input.md
-if (!interactive())
-    args = commandArgs(TRUE)
+VERSION = "0.0.1"
 
-# cat("cwd:", getwd(), "\n")
-fin = args[1]
-setwd(dirname(fin))
-fin = basename(fin)
+"Convert Pandoc's Markdown to PDF.
 
+Usage:
+    pandoc_html <fin> ( [-o <outfp>] | [--output=<outfp>] )
+    pandoc_html <fin> [--pandoc -- <pd_args>...]
+
+Options:
+    -h --help   Show this screen.
+    --version   Show version.
+    -o --output Specify output file path. If not given, inferred
+                from input by changing the file extension.
+    --latex     Output `.tex` instead of `.pdf`.
+    --pandoc    Specify additional arguments to pass to Pandoc.
+" -> DOC
+if (!interactive()) {
+    a = docopt::docopt(DOC, version = VERSION)
+} else {
+    a = docopt::docopt(DOC, args=c("xxx.md"), version = VERSION)
+}
+# print(a)  # View parsed command line arguments
+
+# Determine output path
+fin = tools::file_path_as_absolute(a$fin)
+fout = stom::replace_file_ext(fin,".html")
+fout = file.path(getwd(), basename(fout))  # Generate output to CWD by default
+if (!is.null(a$output))
+    fout = tools::file_path_as_absolute(a$output)
+
+# Compile document
 print("Compile to HTML")
-fout = stom::replace_file_ext(fin, ".html")
-stom::pandoc_html(fin, fout, args[-1])
+setwd(dirname(fin))  # WD as input file's directory
+stom::pandoc_html(fin, fout, unlist(a$pd_args))
+
+
+# Print I/O info
+cat("\n")
+cat("[File I/O]\n")
+stom::lst2message(list(
+    INPUT  = fin,
+    OUTPUT = fout
+), indent=3)
--- inst/cli/pandoc_pdf.R
+++ inst/cli/pandoc_pdf.R
@@ -1,19 +1,50 @@
-# Call as
-#   Rscript  this.R  input.md  [[--latex]]
-if (!interactive())
-    args = commandArgs(TRUE)
+VERSION = "0.0.1"
 
-# cat("cwd:", getwd(), "\n")
-fin = args[1]
-tolatex = stom::cmd_has(args, "--latex")
-setwd(dirname(fin))
-fin = basename(fin)
+"Convert Pandoc's Markdown to PDF.
 
-if (tolatex) {
-    print("Compile to LaTex")
-    fout = stom::replace_file_ext(fin, ".tex")
-    stom::pandoc_tex(fin, fout, style = "amsart", args[-1])
+Usage:
+    pandoc_pdf <fin>
+    pandoc_pdf <fin> [-o <outfp>]
+    pandoc_pdf <fin> [--output=<outfp>]
+    pandoc_pdf <fin> [--latex]
+    pandoc_pdf <fin> [--pandoc -- <pd_args>...]
+
+Options:
+    -h --help   Show this screen.
+    --version   Show version.
+    -o --output Specify output file path. If not given, inferred
+                from input by changing the file extension.
+    --latex     Output `.tex` instead of `.pdf`.
+    --pandoc    Specify additional arguments to pass to Pandoc.
+" -> DOC
+if (!interactive()) {
+    a = docopt::docopt(DOC, version = VERSION)
 } else {
-    fout = stom::replace_file_ext(fin, ".pdf")
-    stom::pandoc_pdf(fin, fout, style = "amsart", args[-1])
+    a = docopt::docopt(DOC, args=c("xxx.md"), version = VERSION)
 }
+# print(a)  # View parsed command line arguments
+
+# Determine output path
+fin = tools::file_path_as_absolute(a$fin)
+fout = stom::replace_file_ext(fin, ifelse(a$latex,".tex",".pdf"))
+fout = file.path(getwd(), basename(fout))  # Generate output to CWD by default
+if (!is.null(a$output))
+    fout = tools::file_path_as_absolute(a$output)
+
+# Compile document
+setwd(dirname(fin))  # WD as input file's directory
+if (a$latex) {
+    message("Compile to LaTex instead of PDF")
+    stom::pandoc_tex(fin, fout, style = "amsart", unlist(a$pd_args))
+} else {
+    stom::pandoc_pdf(fin, fout, style = "amsart", unlist(a$pd_args))
+}
+
+
+# Print I/O info
+cat("\n")
+cat("[File I/O]\n")
+stom::lst2message(list(
+    INPUT  = fin,
+    OUTPUT = fout
+    ), indent=3)
