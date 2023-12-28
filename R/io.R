e708163981797dc4993da2bc18ad4650e808fd87
Yongfu Liao <liao961120@gmail.com>
2023-12-28 15:31:45 +0800

    simplify structure

--- R/io.R
+++ R/io.R
@@ -1,3 +1,11 @@
+SUPPORTED_TYPES = c(
+    "numeric",
+    "complex",
+    "logical",
+    "character",
+    "integer"
+)
+
 #' I/O for data frames and \emph{typed CSV}s
 #'
 #' @param dt A data frame to be written to file.
@@ -20,34 +28,6 @@
 #' xfun::file_string(tmp)  # print file content
 #' read_tcsv(tmp)
 write_tcsv = function(dt, fout) {
-    write_data(dt, fout)
-}
-
-#' @rdname write_tcsv
-#' @export
-read_tcsv = function(fin) {
-    read_data(fin)
-}
-
-
-get_col_type = function(dt) {
-    ctypes = sapply(dt, function(c_) {
-        c_ = class(c_)
-        if ( "ordered" %in% c_ | "factor" %in% c_ )
-            stop("tcsv does not support columns with 'ordered' data type!\n\t",
-                 "Please convert them to either 'character' or 'factor'\n\t",
-                 "before writing to file.")
-        return(c_)
-    })
-    return(ctypes)
-}
-
-# d0 = data.frame(a = 1:6,
-#                 b=c(T,F,T,NA, T,T),
-#                 c=c("a", "", '"', NA, ",", "\t"),
-#                 e = NA_integer_, f = "哈", f2="d" )
-
-write_data = function(dt, fout) {
     ctypes = get_col_type(dt)
     ctypes = paste0("<", ctypes, ">")
     data = apply(dt, 2, function(c_) as.character(c_))
@@ -55,13 +35,13 @@ write_data = function(dt, fout) {
     row.names(out) = NULL
     out = as.data.frame(out)
     readr::write_csv(out, fout, col_names = TRUE,
-                     quote = "all",
-                     escape = "double",
-                     na = "")
+                     quote = "all", escape = "double", na = "")
     return(invisible(out))
 }
 
-read_data = function(fin) {
+#' @rdname write_tcsv
+#' @export
+read_tcsv = function(fin) {
     d = readr::read_csv(fin, col_types = "c",
                         na = "", quote = '"',
                         name_repair = "minimal")
@@ -70,4 +50,17 @@ read_data = function(fin) {
     for (i in seq_along(ctypes))
         class(d[[i]]) = ctypes[i]
     return(d)
+}
+
+get_col_type = function(dt) {
+    ctypes = sapply(dt, function(c_) {
+        c_ = class(c_)
+        if ( "ordered" %in% c_ | "factor" %in% c_ )
+            stop("tcsv does not support columns with 'factor' data type!\n\t",
+                 "Please convert them to 'character' before writing to file.")
+        if (! c_ %in% SUPPORTED_TYPES )
+            stop("tcsv does not support '", c_, "' data type!")
+        return(c_)
+    })
+    return(ctypes)
 }
