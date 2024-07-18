645c5da0689c749819f8241062228b0d1aadd46b
Yongfu Liao <liao961120@gmail.com>
2024-07-18 16:17:56 +0800

    New function: min_max()

--- R/transform.R
+++ R/transform.R
@@ -7,3 +7,13 @@ simplex = function(x) {
   if (any(x < 0)) stop("Input values contain zero!")
   x / sum(x)
 }
+
+#' @rdname simplex
+#' @export
+#' @examples
+#' min_max(-5:10)
+min_max = function(x) {
+    m = min(x)
+    M = max(x)
+    (x - m) / (M - m)
+}
