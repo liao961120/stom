25e7eec0a91d700eb8d582f0f0d00dee43a5096e
Yongfu Liao <liao961120@gmail.com>
2024-08-28 15:21:33 +0800

    new function

--- R/distributions.R
+++ R/distributions.R
@@ -74,3 +74,48 @@ rgampois2 = function (n , mu , phi) {
     rnbinom(n, size = p_n, prob = p_p)
 }
 
+
+#' Random Number Generator for Cholesky LKJ Correlation Distribution
+#'
+#' @param n Number of samples.
+#' @param K Dimension of matrix
+#' @param eta Parameter controling the strengths of correlations
+#' @export
+#' @details
+#' Function taken from https://github.com/rmcelreath/rethinking/blob/master/R/distributions.r#L214
+rlkjcorr <- function ( n , K , eta = 1 ) {
+
+    stopifnot(is.numeric(K), K >= 2, K == as.integer(K))
+    stopifnot(eta > 0)
+    #if (K == 1) return(matrix(1, 1, 1))
+
+    f <- function() {
+        alpha <- eta + (K - 2)/2
+        r12 <- 2 * rbeta(1, alpha, alpha) - 1
+        R <- matrix(0, K, K) # upper triangular Cholesky factor until return()
+        R[1,1] <- 1
+        R[1,2] <- r12
+        R[2,2] <- sqrt(1 - r12^2)
+        if(K > 2) for (m in 2:(K - 1)) {
+            alpha <- alpha - 0.5
+            y <- rbeta(1, m / 2, alpha)
+
+            # Draw uniformally on a hypersphere
+            z <- rnorm(m, 0, 1)
+            z <- z / sqrt(crossprod(z)[1])
+
+            R[1:m,m+1] <- sqrt(y) * z
+            R[m+1,m+1] <- sqrt(1 - y)
+        }
+        return(crossprod(R))
+    }
+    R <- replicate( n , f() )
+    if ( dim(R)[3]==1 ) {
+        R <- R[,,1]
+    } else {
+        # need to move 3rd dimension to front, so conforms to array structure that Stan uses
+        R <- aperm(R,c(3,1,2))
+    }
+    return(R)
+}
+
