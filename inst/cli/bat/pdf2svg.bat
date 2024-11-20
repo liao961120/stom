4d8b3a99c9b1348abbdbe232f36587d76ca73e63
Yongfu Liao <liao961120@gmail.com>
2024-11-20 14:49:43 +0800

    update

--- inst/cli/bat/pdf2svg.bat
+++ inst/cli/bat/pdf2svg.bat
@@ -1,3 +1,11 @@
-@ECHO OFF
-Rscript C:\Users\rd\AppData\Local\Programs\R\R-4.1.3\library\stom\cli\pdf2img.R %* --to=svg
+@echo off
+REM Adjust the path to your Rscript executable and the script file
+set RscriptPath="C:\Users\tomliao\AppData\Local\Programs\R\R-4.2.3\bin\Rscript.exe"
+set ScriptPath="C:\Users\tomliao\AppData\Local\Programs\R\R-4.2.3\library\stom\cli\pdf2svg.R"
 
+if "%1"=="" (
+    echo Usage: pdf2svg input.pdf [-o output.svg]
+    exit /b 1
+)
+
+%RscriptPath% %ScriptPath% %*
--- inst/cli/bat/svg2png.bat
+++ inst/cli/bat/svg2png.bat
@@ -1,10 +1,10 @@
 @echo off
 REM Adjust the path to your Rscript executable and the script file
 set RscriptPath="C:\Users\tomliao\AppData\Local\Programs\R\R-4.2.3\bin\Rscript.exe"
-set ScriptPath="C:\Users\tomliao\AppData\Local\Programs\R\R-4.2.3\library\stom\cli\pdf2png.R"
+set ScriptPath="C:\Users\tomliao\AppData\Local\Programs\R\R-4.2.3\library\stom\cli\svg2png.R"
 
 if "%1"=="" (
-    echo Usage: pdf2png input.pdf [-o output.png]
+    echo Usage: svg2png input.svg [-o output.png]
     exit /b 1
 )
 
